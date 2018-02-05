578799c26d61e9bda65f4b5e66858c1ff1616acd
usermdash <email2dash@gmail.com>
2018-02-05 21:50:52 +0530

    Test

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,27 @@
-console.log("Hello Wordl!");
+/*Your Code Goes Here*/
+
+
+
+function printFullAge(years) {
+    var ages = [];
+    var fullAge = [];
+
+    for (var i = 0; i < years.length; i++) {
+        ages[i] = 2018 - years[i];
+
+        if (ages[i] >= 18) {
+            console.log('Person ' + (i + 1) + ' Is ' + ages[i] + ' Years Old. AND It\'s Full Age');
+            fullAge.push(true);
+        } else {
+            console.log('Person ' + (i + 1) + ' Is ' + ages[i] + ' Years Old. AND It\'s NOT Full Age');
+            fullAge.push(false);
+        }
+
+    }
+    return fullAge;
+}
+
+var years = [1990, 1995, 2010, 2000, 2001];
+
+var full_1 = printFullAge(years);
+var full_2 = printFullAge([2000, 2001, 1999, 1995]);
